b5945f377ce4b121cc7354d9c9c55054565ea017
lmonty22 <lindsay.montgomery@olympian.org>
2020-03-16 13:50:04 -0400

    Done.

---
+++ lib/concerns/memorable.rb
@@ -0,0 +1,19 @@
+module Memorable 
+
+    module InstanceMethods
+        def initialize
+            self.class.all << self
+        end  
+    end
+
+    module ClassMethods 
+        def count 
+            self.all.count
+        end
+        
+        def reset_all
+            self.all.clear
+        end
+    end 
+    
+end
